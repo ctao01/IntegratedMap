b03a398d52f13d6b2c7c645a2d0e6aaa2781dbe2
joycytao <ctao01.poly@gmail.com>
2012-04-06 22:01:19 -0500

    Initial Commit

---
+++ Map_v3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Map_v3
+//
+//  Created by Joy Tao on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
